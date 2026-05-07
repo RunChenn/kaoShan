14d3ee8260638acb10c466a290fbb8dc0b002066
Run Chen <yma8882003@gmail.com>
2026-05-07 11:17:40 +0800

    add: VITE_API_BASE_URL

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,8 +1,10 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/v1'
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8000/api/v1',
+  baseURL: apiBaseUrl,
   timeout: 10000,
 })
 
